5be3bde9e07834a1d035072397eb8f162b530c26
coskunil <coskunil_06@hotmail.com>
2024-07-20 14:56:13 +0300

    a

--- src/screens/Home/Main.js
+++ src/screens/Home/Main.js
@@ -8,8 +8,6 @@ import {
   TextInput,
   Image,
   StatusBar,
-  ScrollView,
-  BackHandler,
   FlatList,
 } from 'react-native';
 import {height, hp, RFValue, wp} from '../../helpers/responsive';
@@ -18,7 +16,7 @@ import store from '../../store/store';
 import {KonsolCard} from '../../components';
 import Favorite from './Favorite';
 
-const Main = observer(({navigation: {navigate, goBack}}) => {
+const Main = observer(({navigation: {navigate}}) => {
   const bigdata = [
     {
       id: 1,
@@ -75,9 +73,9 @@ const Main = observer(({navigation: {navigate, goBack}}) => {
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
-  const flatListRef = useRef(null);
-
   const [selectedFilterIndex, setSelectedFilterIndex] = useState(0);
+
+  const flatListRef = useRef(null);
 
   const onViewableItemsChanged = useRef(({viewableItems}) => {
     if (viewableItems.length > 0) {
@@ -87,188 +85,195 @@ const Main = observer(({navigation: {navigate, goBack}}) => {
 
   return (
     <View style={styles.container}>
-    <StatusBar
-      backgroundColor={'transparent'}
-      translucent
-      barStyle={'dark-content'}
-    />
+      <StatusBar
+        backgroundColor={'transparent'}
+        translucent
+        barStyle={'dark-content'}
+      />
 
-    <View style={styles.oneView}>
-      <Image
-        source={images.threeline}
-        style={styles.threeIcon}
-        resizeMode="contain"
-      />
-      <Image
-        source={images.anybuylogo}
-        style={styles.anybuyIcon}
-        resizeMode="contain"
-      />
-      <View>
+      <View style={styles.oneView}>
         <Image
-          source={images.ar}
-          style={styles.arIcon}
+          source={images.threeline}
+          style={styles.threeIcon}
           resizeMode="contain"
         />
-        <Text style={{color: 'black', alignSelf: 'center'}}>AR</Text>
+        <Image
+          source={images.anybuylogo}
+          style={styles.anybuyIcon}
+          resizeMode="contain"
+        />
+        <View>
+          <Image
+            source={images.ar}
+            style={styles.arIcon}
+            resizeMode="contain"
+          />
+          <Text style={{color: 'black', alignSelf: 'center'}}>AR</Text>
+        </View>
       </View>
-    </View>
-    <Text style={styles.lets}>let’s Find Your Item!</Text>
-    <View style={styles.searchView}>
-      <TextInput
-        style={styles.searchTextInput}
-        placeholder={'Search'}
-        placeholderTextColor={'black'}
-      />
-      <Image
-        source={images.search}
-        style={styles.searchIcon}
-        resizeMode="contain"
-      />
-      <TouchableOpacity
-        onPress={() => {
-          navigate('Scan');
-        }}>
+      
+      <Text style={styles.lets}>let’s Find Your Item!</Text>
+
+      <View style={styles.searchView}>
+        <TextInput
+          style={styles.searchTextInput}
+          placeholder={'Search'}
+          placeholderTextColor={'black'}
+        />
         <Image
-          source={images.camera}
-          style={styles.cameraIcon}
+          source={images.search}
+          style={styles.searchIcon}
           resizeMode="contain"
         />
-      </TouchableOpacity>
-    </View>
-    <View style={styles.oneView}>
-      <Text style={styles.nearbyText}>Nearby</Text>
-      <TouchableOpacity
-        onPress={() => navigate('ProfileDetail')}
-        style={styles.oneTouchable}>
-        <Image
-          source={images.three}
-          style={{height: hp(2.5), width: wp(5)}}
-          resizeMode="contain"
-        />
-      </TouchableOpacity>
-    </View>
-    <FlatList
-      style={{flexGrow: 0}}
-      contentContainerStyle={{
-        paddingLeft: wp(3),
-        paddingRight: wp(4),
-        gap: wp(5.5),
-      }}
-      showsHorizontalScrollIndicator={false}
-      horizontal
-      data={bigdata}
-      keyExtractor={item => item.id.toString()}
-      renderItem={({item, index}) => (
         <TouchableOpacity
           onPress={() => {
-            navigate('Detail');
+            navigate('Scan');
           }}>
           <Image
-            source={item?.imagee}
-            style={{
-              zIndex: 1,
-              height: hp(30),
-              width: wp(90),
-              marginTop: hp(6.5),
-            }}
+            source={images.camera}
+            style={styles.cameraIcon}
             resizeMode="contain"
           />
         </TouchableOpacity>
-      )}
-      onViewableItemsChanged={onViewableItemsChanged}
-      viewabilityConfig={{viewAreaCoveragePercentThreshold: 50}}
-    />
-    <View
-      style={{
-        flexDirection: 'row',
-        justifyContent: 'center',
-        marginTop: hp(1),
-      }}>
-      {bigdata.map((_, index) => (
-        <View
-          key={index}
-          style={{
-            height: hp(0.6),
-            width: currentIndex === index ? wp(2) : wp(10),
-            borderRadius: wp(2),
-            backgroundColor: '#FF9900',
-            marginHorizontal: wp(0.2),
-          }}
-        />
-      ))}
-    </View>
-    <FlatList
-      style={{flexGrow: 0}}
-      contentContainerStyle={{marginTop: hp(4), gap: wp(3.5)}}
-      showsHorizontalScrollIndicator={false}
-      horizontal
-      data={datatext}
-      keyExtractor={item => item.id.toString()}
-      renderItem={({item, index}) => {
-        const isSelected = index === selectedFilterIndex;
-        return (
-          <View key={item.id}>
-            <TouchableOpacity
-              onPress={() => {
-                setSelectedFilterIndex(index);
-              }}>
-              <Text
-                style={[
-                  styles.oneFlatlistText,
-                  isSelected ? {color: '#FF9900'} : null,
-                ]}>
-                {item.text}
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={[
-                styles.oneFlatlistTouchable,
-                isSelected ? {backgroundColor: '#FF9900'} : null,
-              ]}
+      </View>
+
+      <View style={styles.oneView}>
+        <Text style={styles.nearbyText}>Nearby</Text>
+        <TouchableOpacity
+          onPress={() => navigate('ProfileDetail')}
+          style={styles.oneTouchable}>
+          <Image
+            source={images.three}
+            style={{height: hp(2.5), width: wp(5)}}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        style={{flexGrow: 0}}
+        contentContainerStyle={{
+          paddingLeft: wp(3),
+          paddingRight: wp(4),
+          gap: wp(5.5),
+        }}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        data={bigdata}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            onPress={() => {
+              navigate('Detail');
+            }}>
+            <Image
+              source={item?.imagee}
+              style={{
+                zIndex: 1,
+                height: hp(30),
+                width: wp(90),
+                marginTop: hp(6.5),
+              }}
+              resizeMode="contain"
             />
-          </View>
-        );
-      }}
-    />
-    <FlatList
-      contentContainerStyle={{
-        paddingLeft: wp(5),
-        paddingRight: wp(5),
-        gap: wp(5.5),
-      }}
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      data={konsol}
-      keyExtractor={item => item.id.toString()}
-      renderItem={({item, index}) => (
-        <KonsolCard
-          key={index}
-          imagee={item.imagee}
-          name={item.name}
-          price={item.price}
-        />
-      )}
-    />
-    <View style={styles.line}></View>
-    <FlatList
-      style={styles.twoFlatlist}
-      contentContainerStyle={{paddingBottom: hp(35)}}
-      numColumns={2}
-      showsVerticalScrollIndicator={false}
-      data={store.userList}
-      keyExtractor={item => item.id.toString()}
-      renderItem={({item, index}) => (
-        <UserCard
-          key={index}
-          userImage={item.userImage}
-          userName={item.userName}
-          userGender={item.userGender}
-          userAge={item.userAge}
-        />
-      )}
-    />
-  </View>
+          </TouchableOpacity>
+        )}
+        onViewableItemsChanged={onViewableItemsChanged}
+        viewabilityConfig={{viewAreaCoveragePercentThreshold: 50}}
+      />
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          marginTop: hp(1),
+        }}>
+        {bigdata.map((_, index) => (
+          <View
+            key={index}
+            style={{
+              height: hp(0.6),
+              width: currentIndex === index ? wp(2) : wp(10),
+              borderRadius: wp(2),
+              backgroundColor: '#FF9900',
+              marginHorizontal: wp(0.2),
+            }}
+          />
+        ))}
+      </View>
+
+      <FlatList
+        style={{flexGrow: 0}}
+        contentContainerStyle={{marginTop: hp(4), gap: wp(3.5)}}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        data={datatext}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item, index}) => {
+          const isSelected = index === selectedFilterIndex;
+          return (
+            <View key={item.id}>
+              <TouchableOpacity
+                onPress={() => {
+                  setSelectedFilterIndex(index);
+                }}>
+                <Text
+                  style={[
+                    styles.oneFlatlistText,
+                    isSelected ? {color: '#FF9900'} : null,
+                  ]}>
+                  {item.text}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[
+                  styles.oneFlatlistTouchable,
+                  isSelected ? {backgroundColor: '#FF9900'} : null,
+                ]}
+              />
+            </View>
+          );
+        }}
+      />
+
+      <FlatList
+        contentContainerStyle={{
+          paddingLeft: wp(5),
+          paddingRight: wp(5),
+          gap: wp(5.5),
+        }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={konsol}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item}) => (
+          <KonsolCard
+            imagee={item.imagee}
+            name={item.name}
+            price={item.price}
+          />
+        )}
+      />
+
+      <View style={styles.line}></View>
+
+      <FlatList
+        style={styles.twoFlatlist}
+        contentContainerStyle={{paddingBottom: hp(35)}}
+        numColumns={2}
+        showsVerticalScrollIndicator={false}
+        data={store.userList}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item}) => (
+          <UserCard
+            userImage={item.userImage}
+            userName={item.userName}
+            userGender={item.userGender}
+            userAge={item.userAge}
+          />
+        )}
+      />
+    </View>
   );
 });
 
@@ -309,6 +314,7 @@ const styles = StyleSheet.create({
     width: wp(90),
     fontFamily: FONTS.medium,
     fontSize: RFValue(19),
+    marginTop: hp(2),
   },
   searchView: {
     flexDirection: 'row',
@@ -336,14 +342,6 @@ const styles = StyleSheet.create({
     height: hp(7),
     width: wp(14),
     marginTop: hp(2),
-  container: {},
-  oneView: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    width: wp(90),
-    alignSelf: 'center',
-    marginTop: hp(6.5),
   },
   nearbyText: {
     color: 'black',
@@ -357,12 +355,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: wp(100),
-  },
-  oneFlatlist: {
-    marginTop: hp(1.6),
-    gap: wp(8),
-    paddingLeft: wp(5),
-    paddingRight: wp(5),
   },
   oneFlatlistText: {
     fontSize: RFValue(17),
